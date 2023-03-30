312464018e38eccc188f661a3d5eb2605b05c605
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2023-03-31 00:33:46 +0200

    Fix a comment for game()
    Comment turned out to be a bit misleading, changed it
    be more general

--- script.js
+++ script.js
@@ -51,37 +51,49 @@ function playRound(playerSelection, computerSelection){
     return winner;
 }
 
-// create function game() that plays 5 round game, keeps winner and reports a winner
+// create function game() that plays X round game, keeps score and reports a winner
 function game(rounds) {
+
     let playerResult = 0;
     let computerResult = 0;
+
     // call playRound "rounds" times
     for(let i = 0; i < rounds; i++){
 
+        //Take input from the player
         let playerChoice = prompt("Choose Rock, Paper or Scissors: ");
-        let computerChoice = getComputerChoice();
-        let score = playRound(playerChoice, computerChoice);
 
+        //Take computer's choice
+        let computerChoice = getComputerChoice();
+
+        //Play round
+        let score = playRound(playerChoice, computerChoice);
+        // 
         if (score === "player"){
             playerResult++;
         }
         else if(score === "computer"){
             computerResult++;
         }
+
+        // If draw, round is not resolved
         else if(score === "draw"){
             rounds++;
         }
+
+        // check for input validity
         else{
             console.log("Type only 'rock', 'paper' or 'scissors'");
             rounds++;
         }
-
+        // Display current scores
         console.log("Computer chose " + computerChoice);
         console.log("Player: " + playerResult);
         console.log("Computer: " + computerResult);
         console.log("\n");
     }
 
+    // Display final result
     if(playerResult > computerResult){
         console.log("PLAYER WINS");
     }
