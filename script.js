119a9e3ded0631e003d7f8b1c51ac9681f66e0f6
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2023-03-31 12:56:12 +0200

    Change logic of the game for first to reach
    In previous version hard coded number of rounds
    was played no matter what. Changed it so now
    game ends when one of the players reaches
    predetermined score (default: 5).

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// create bot that will randomly choose rock, paper or scissors
+// Create bot that will randomly choose rock, paper or scissors
 
 function getComputerChoice(){
     let options = ["rock", "paper", "scissors"];
@@ -8,11 +8,11 @@ function getComputerChoice(){
     return choice;
 }
 
-// create function that plays round so it takes playerSelection and computerSelection and return a winner in string
+// Create function that plays round so it takes playerSelection and computerSelection and return a winner in string
 
 function playRound(playerSelection, computerSelection){
 
-    // make it case insensitive
+    // Make it case insensitive
 
     playerSelection = playerSelection.toLowerCase();
     let winner;
@@ -51,24 +51,26 @@ function playRound(playerSelection, computerSelection){
     return winner;
 }
 
-// create function game() that plays X round game, keeps score and reports a winner; First to "rounds" is a winner
-function game(rounds) {
+// Create function game() that plays X round game, keeps score and reports a winner; First to "rounds" is a winner
+function game(finalResult) {
 
     let playerResult = 0;
     let computerResult = 0;
 
-    // call playRound "rounds" times
-    for(let i = 0; i < rounds; i++){
+    // Call playRound "rounds" times
+    let keepGoing = true;
+    while(keepGoing){
 
-        //Take input from the player
+        // Take input from the player
         let playerChoice = prompt("Choose Rock, Paper or Scissors: ");
 
-        //Take computer's choice
+        // Take computer's choice
         let computerChoice = getComputerChoice();
 
-        //Play round
+        // Play round
         let score = playRound(playerChoice, computerChoice);
-        // 
+
+        // Keep track of scores
         if (score === "player"){
             playerResult++;
         }
@@ -78,31 +80,29 @@ function game(rounds) {
 
         // If draw, round is not resolved
         else if(score === "draw"){
-            rounds++;
+            console.log("Draw");
         }
 
-        // check for input validity
+        // Check for input validity
         else{
             console.log("Type only 'rock', 'paper' or 'scissors'");
-            rounds++;
         }
         // Display current scores
         console.log("Computer chose " + computerChoice);
         console.log("Player: " + playerResult);
         console.log("Computer: " + computerResult);
         console.log("\n");
-    }
 
-    // Display final result
-    if(playerResult > computerResult){
-        console.log("PLAYER WINS");
-    }
-    else if(playerResult < computerResult){
-        console.log("COMPUTER WINS");
-    }
-    else{
-        console.log("DRAW");
-    }
+                // Display final result
+        if(playerResult === finalResult){
+            console.log("PLAYER WINS");
+            keepGoing = false;
+        }
+        else if(computerResult === finalResult){
+            console.log("COMPUTER WINS");
+            keepGoing = false;
+        }
+        }
 }
 
 game(5);
